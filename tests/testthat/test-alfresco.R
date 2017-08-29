a376b204beeea4f7dcfc8de5f54ffb001cd6c035
leonawicz <matt.rstat@gmail.com>
2017-08-29 15:35:59 -0600

    Add lintr unit test.

--- tests/testthat/test-alfresco.R
+++ tests/testthat/test-alfresco.R
@@ -1,10 +1,10 @@
 context("alfresco")
 
 test_that("rvtable class and attributes preserved", {
-  x <- alf_defaults()
+  x <- alfdef()
 
   expect_is(x, "list")
-  expect_equal(x$atlas_shiny_dir, "/big_scratch/shiny")
+  expect_equal(x$atlas_shiny_dir, "/big_scratch/shiny") # nolint start
   expect_equal(x$atlas_run_owner_dir, "mfleonawicz@alaska.edu")
-  expect_equal(x$age_spinups, "/big_scratch/mfleonawicz/Alf_Files_20121129/Spinup300Year_32Reps/Age_0_1900.tif")
+  expect_equal(x$age_spinups, "/big_scratch/mfleonawicz/Alf_Files_20121129/Spinup300Year_32Reps/Age_0_1900.tif") # nolint end
 })
