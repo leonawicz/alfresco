9ca2d907dda6f582d42f6d691d645b6389de023c
leonawicz <matt.rstat@gmail.com>
2017-08-28 18:15:25 -0600

    Add and document functions for ALFRESCO-related defaults and copying outputs from one run to be used as inputs to a subsequent run.

---
+++ R/alfresco.R
@@ -0,0 +1,19 @@
+#' Common default values
+#'
+#' Common default values in the context of ALFRESCO modeling.
+#'
+#' This function returns a list of default values like standard file paths on the SNAP Atlas cluster
+#' for running the model and processing outputs and paths to standard input maps.
+#'
+#' @return a named list.
+#' @export
+#'
+#' @examples
+#' alf_defaults()$atlas_shiny_dir
+alf_defaults <- function(){
+  list(
+    atlas_shiny_dir = "/big_scratch/shiny",
+    atlas_run_owner_dir = "mfleonawicz@alaska.edu",
+    age_spinups = "/big_scratch/mfleonawicz/Alf_Files_20121129/Spinup300Year_32Reps/Age_0_1900.tif"
+  )
+}
---
+++ R/outputs_as_inputs.R
@@ -0,0 +1,50 @@
+#' Copy ALFRESCO output maps
+#'
+#' Copy ALFRESCO output maps to new location for use as inputs to subsequent model runs.
+#'
+#' This function copies ALFRESCO output maps from a single, "final" year where \code{year} is chosen to be
+#' one less than the starting year of the subsequent model run. For example, a run beginning in 1950
+#' would use as inputs 1949 output maps from a prior model run.
+#' \code{year} need not be the final year of the original run, but it is final
+#' in the context that the following year will be part of a new, subsequent run.
+#' The output directory is at alf_defaults()$atlas_shiny_dir and begins with \code{"/Final_"}.
+#'
+#' @param domain character, usually \code{"Statewide"} or \code{"Noatak"}.
+#' @param run_name character.
+#' @param year numeric, the "final" year of a run. See details.
+#' @param suffix appended to output directory, defaults to \code{"runs"}.
+#' @param sub_dir name of directory where copied maps are stored, defaults to \code{"secondaryRunInputs"}.
+#' @param mc.cores number of processors, defaults to 32.
+#'
+#' @return file copy side effect.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' outputs_as_inputs("Statewide", "fmo00s00i_historical_CRU32", 1949)
+#' outputs_as_inputs("Statewide", "fmo00s00i_historical_CRU32", 2013)
+#' }
+outputs_as_inputs <- function(domain, run_name, year, suffix = "runs",
+                              sub_dir = "secondaryRunInputs", mc.cores = 32){
+  top_dir <- alf_defaults()$atlas_shiny_dir
+  owner <- alf_defaults()$atlas_run_owner_dir
+  in_dir <- paste0(top_dir, "/Runs_", domain, "/", owner, "/", run_name, "/Maps")
+  dir.create(
+    out_dir <- paste0(top_dir, "/Final_", domain, "_", suffix, "/", sub_dir),
+    showWarnings = FALSE, recursive = TRUE)
+  files <- list.files(in_dir, pattern = paste0(year, "\\.tif"), full.names = TRUE)
+  files <- files[-which(substr(basename(files), 1, 8) == "FireScar")]
+  r <- raster::raster(alf_defaults()$age_spinups)
+  parallel::mclapply(seq_along(files), .write_outputs_as_inputs,
+                     template = template, out_dir = out_dir, mc.cores = mc.cores)
+}
+
+.write_outputs_as_inputs <- function(i, template, out_dir){
+  r <- raster::raster(files[i])
+  r <- raster::extend(r, template)
+  if(substr(basename(files[i]), 1, 3) == "Age")
+    raster::writeRaster(r, file.path(out_dir, basename(files[i])), datatype = "INT4S", overwrite = TRUE)
+  if(substr(basename(files[i]), 1, 3) != "Age")
+    raster::writeRaster(r, file.path(out_dir, basename(files[i])), datatype = "INT1U", overwrite = TRUE)
+  return()
+}
