7c79f615b83cfb7d44cca75fd7ed5359e6bf56df
leonawicz <matt.rstat@gmail.com>
2017-08-29 15:16:39 -0600

    Add post-processing functions for ALFRESCO fire events from map outputs.

---
+++ R/postproc.R
@@ -0,0 +1,106 @@
+#' ALFRESCO post-processing of fire events
+#'
+#' These functions are used for post-processing of ALFRESCO fire events from map outputs.
+#'
+#' @param k integer, an iterator.
+#' @param b a raster brick.
+#' @param pts table of point locations.
+#' @param locs location names
+#' @param replicates character, simulation replicate labels.
+#' @param source character, \code{"Observed"} or \code{"Modeled"}.
+#' @param buffer.list list of point location buffers in meters.
+#' @param buffer.labels labels for buffers in list.
+#' @param burnable.cells.raster binary raster of possibly burnable cells.
+#' @param mainDir directory path.
+#'
+#' @return a list.
+fireEventsFun <- function(k, pts, locs, replicates, source="Modeled", buffer.list = list(NULL),
+                          buffer.labels = LETTERS[1:length(buffer.list)],
+                          burnable.cells.raster = NULL, mainDir){
+  if(!is.null(burnable.cells.raster))
+    burnable.cells <- raster::Which(burnable.cells.raster == 1)
+  reps <- paste0("_", k-1, "_")
+  files <- list.files(mainDir, pattern =
+                        gsub("expression", "",
+                             paste(bquote(expression("^FireSc.*.", .(reps), ".*.tif$")), collapse="")),
+                      full.names = TRUE)
+  yrs <- as.numeric(gsub("FireScar_\\d+_", "", gsub(".tif", "", basename(files))))
+  n <- length(yrs)
+  ord <- order(yrs)
+  files <- files[ord]
+  yrs <- yrs[ord]
+  d <- vector("list", n)
+  for(i in 1:n){
+    r <- raster::raster(files[i])
+    r[is.na(r)] <- 0
+    if(i == 1) r <- raster::mask(r, burnable.cells, maskvalue = 0)
+    r[r > 0] <- 1
+    if(i == 1){
+      r.hold <- r
+      cells <- vector("list", length(buffer.list))
+      for(p in seq_aloing(buffer.list)){
+        if(is.null(buffer.list[[p]])){
+          tmp <- as.list(extract(r, pts, cellnumbers = TRUE)[,1])
+        } else {
+          tmp <- purrr::map(extract(r, pts, buffer=buffer.list[[p]], cellnumbers = TRUE), .x[, 1])
+        }
+        tmp <- purrr::map(seq_along(tmp), ~tibble::data_frame(Location=locs[.x], Cell=tmp[[.x]])) %>%
+          dplyr::bind_rows()
+        cells[[p]] <- dplyr::mutate(tmp, Buffer_km = buffer.labels[p])
+      }
+      cells <- dplyr::bind_rows(cells)
+    } else r.hold <- r.hold + r
+    cells <- dplyr::mutate(cells, Value=r[Cell])
+    d[[i]] <- dplyr::group_by(cells, Buffer_km, Location) %>%
+      dplyr::summarise(Value = mean(Value, na.rm=TRUE)) %>% dplyr::mutate(Year = yrs[i])
+  }
+  d <- dplyr::bind_rows(d) %>% dplyr::mutate(
+    Source = factor(source, levels=c("Observed", "Modeled")),
+    Replicate = factor(replicates[k], levels = unique(c("Observed", replicates)))) %>%
+    dplyr::ungroup() %>% dplyr::arrange(Source, Replicate, Buffer_km, Location, Year) %>%
+    dplyr::select(Source, Replicate, Buffer_km, Location, Year, Value)
+  list(rasters = r.hold, points = d, years = n, years.vec = yrs)
+}
+
+#' @export
+#' @rdname fireEventsFun
+fireEventsFunEmpirical <- function(b, pts, locs, replicates = "Observed", source = "Observed",
+                                   buffer.list = list(NULL), buffer.labels = LETTERS[1:length(buffer.list)],
+                                   burnable.cells.raster = NULL){
+  if(!is.null(burnable.cells.raster))
+    burnable.cells <- raster::Which(burnable.cells.raster == 1)
+  n <- nlayers(b)
+  d <- vector("list", n)
+  for(i in 1:n){
+    r <- subset(b, i)
+    r[is.na(r)] <- 0
+    if(i == 1) r <- raster::mask(r, burnable.cells, maskvalue = 0)
+    r[r > 0] <- 1
+    if(i == 1){
+      r.hold <- r
+      cells <- vector("list", length(buffer.list))
+      for(p in seq_along(buffer.list)){
+        if(is.null(buffer.list[[p]])){
+          tmp <- as.list(extract(r, pts, cellnumbers = TRUE)[,1])
+        } else {
+          tmp <- purrr::map(raster::extract(
+            r, pts, buffer = buffer.list[[p]], cellnumbers = TRUE), ~.x[, 1])
+        }
+        tmp <- purrr::map(
+          seq_along(tmp), tibble::data_frame(Location = locs[.x], Cell = tmp[[.x]])) %>%
+          dplyr::bind_rows()
+        cells[[p]] <- dplyr::mutate(tmp, Buffer_km = buffer.labels[p])
+      }
+      cells <- dplyr::bind_rows(cells)
+    } else r.hold <- r.hold + r
+    cells <- dplyr::mutate(cells, Value = r[Cell])
+    d[[i]] <- dplyr::group_by(cells, Buffer_km, Location) %>%
+      dplyr::summarise(Value = mean(Value, na.rm=TRUE)) %>% dplyr::mutate(Year = yrs[i])
+  }
+  d <- dplyr::bind_rows(d) %>% dplyr::mutate(
+    Source = factor(source, levels=c("Observed", "Modeled")),
+    Replicate = factor(replicates[1], levels=unique(c("Observed", replicates)))) %>%
+    dplyr::ungroup() %>% dplyr::arrange(Source, Replicate, Buffer_km, Location, Year) %>%
+    dplyr::select(Source, Replicate, Buffer_km, Location, Year, Value)
+  list(rasters = r.hold, points = d, years = n)
+}
