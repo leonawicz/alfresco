423d6bec3c332f06853bf2346cb2bb49fd4f17e8
leonawicz <matt.rstat@gmail.com>
2017-08-29 15:15:53 -0600

    Add commandline helpers.

---
+++ R/commandline.R
@@ -0,0 +1,26 @@
+#' Prepare command line arguments
+#'
+#' Prepare command line argument inputs in a matrix.
+#'
+#' @param comArgs output from \code{commandArgs(TRUE)}.
+#'
+#' @return a matrix.
+#' @export
+#'
+#' @examples
+prep_comArgs <- function(comArgs){
+  x <- do.call("rbind", strsplit(comArgs, "="))
+  options(warn = -1)
+  arg.char <- which(is.na(as.numeric(x[, 2])))
+  options(warn=0)
+  if(length(arg.char) > 0)
+    x[arg.char, 2] <- paste0("'", x[arg.char,2], "'")
+  x
+}
+
+prep_alf_frp <- function(comArgs, out){
+  cat(comArgs)
+  dir.create(file.path(out, "FRP"), showWarnings = FALSE)
+  sink(file = file.path(out, "message.txt"), append = TRUE)
+  cat("Below is a link to a preliminary R Shiny Alfresco FRP/FRI results app.\n")
+}
