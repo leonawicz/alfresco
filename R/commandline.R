54c7dfd6c7dcae3eb3ef707aac8ac553f1bf52cd
leonawicz <matt.rstat@gmail.com>
2017-08-29 15:47:46 -0600

    Update function and documentation.

--- R/commandline.R
+++ R/commandline.R
@@ -19,9 +19,22 @@ prep_comArgs <- function(comArgs){
   x
 }
 
+#' Helper function for FRP processing
+#'
+#' Helper function for Fire Return Period processing.
+#'
+#' @param comArgs
+#' @param out
+#'
+#' @return side effects of creating output directory, \code{sink}-ing message.
+#' @export
+#'
+#' @examples
+#' # not run
 prep_alf_frp <- function(comArgs, out){
   cat(comArgs)
   dir.create(file.path(out, "FRP"), showWarnings = FALSE)
   sink(file = file.path(out, "message.txt"), append = TRUE)
   cat("Below is a link to a preliminary R Shiny Alfresco FRP/FRI results app.\n")
+  sink()
 }
