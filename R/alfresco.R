6ed561c6a76ab7ce2f34671ffd77927e5fef5fcd
leonawicz <matt.rstat@gmail.com>
2017-09-01 16:09:05 -0600

    Update globals and defaults.

--- R/alfresco.R
+++ R/alfresco.R
@@ -1,4 +1,4 @@
-globalVariables(c(".x", "BUffer_km"))
+globalVariables(c(".x", "Domain", "Replicate", "Source", "BUffer_km", "Vegetation"))
 
 #' alfresco: R functions for ALFRESCO wildfire model.
 #'
@@ -32,7 +32,14 @@ alfdef <- function(){
     atlas_run_owner_dir = "mfleonawicz@alaska.edu",
     age_spinups = "/big_scratch/mfleonawicz/Alf_Files_20121129/Spinup300Year_32Reps/Age_0_1900.tif",
     fire_cause_lightning="/big_scratch/mfleonawicz/FAH/FireAreaHistory_11182013.shp",
-    fire_cause_all="/big_scratch/mfleonawicz/FAH/Lightning_Fires_11182013.shp"
+    fire_cause_all="/big_scratch/mfleonawicz/FAH/Lightning_Fires_11182013.shp",
+    eris_apps_dir="/var/www/shiny-server/shiny-apps",
+    eris_template_app_dir="/var/www/shiny-server/shiny-apps/alfoutdev",
+    eris_alf_dir="/var/www/shiny-server/shiny-apps/alfresco",
+    noa_veg="/big_scratch/mfleonawicz/Alf_Files_20121129/alf2005.cavm.merged.030212_Noatak.tif",
+    sw_veg="/big_scratch/mfleonawicz/Alf_Files_20121129/alf2005.cavm.merged.030212.tif",
+    noa_shp="/big_scratch/mfleonawicz/Alf_Files_20121129/noa_basin2/Noa_basin2.shp",
+    sw_shp="/big_scratch/mfleonawicz/Alf_Files_20121129/statewide_shape/Alaska_Albers_ESRI.shp"
   )
 }
 # nolint end
