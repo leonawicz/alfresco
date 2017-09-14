556ff09d16b8e83b890ed4835de248c49c413b4f
leonawicz <matt.rstat@gmail.com>
2017-09-14 11:21:27 -0600

    Update defaults.

--- R/alfresco.R
+++ R/alfresco.R
@@ -13,7 +13,6 @@ NULL
 #' @importFrom magrittr %>%
 NULL
 
-# nolint start
 #' Common default values
 #'
 #' Common default values in the context of ALFRESCO modeling.
@@ -27,20 +26,23 @@ NULL
 #' @examples
 #' alfdef()$atlas_shiny_dir
 alfdef <- function(){
+  bsalf <- "/big_scratch/mfleonawicz/Alf_Files_20121129" # nolint start
+  alfex <- "/atlas_scratch/mfleonawicz/alfresco"
+  proj <- "/atlas_scratch/mfleonawicz/projects"
   list(
     atlas_shiny_dir = "/big_scratch/shiny",
     atlas_run_owner_dir = "mfleonawicz@alaska.edu",
-    age_spinups = "/big_scratch/mfleonawicz/Alf_Files_20121129/Spinup300Year_32Reps/Age_0_1900.tif",
+    age_spinups = file.path(bsalf, "Spinup300Year_32Reps/Age_0_1900.tif"),
     fire_cause_lightning = "/big_scratch/mfleonawicz/FAH/FireAreaHistory_11182013.shp",
     fire_cause_all = "/big_scratch/mfleonawicz/FAH/Lightning_Fires_11182013.shp",
     eris_apps_dir = "/var/www/shiny-server/shiny-apps",
     eris_template_app_dir = "/var/www/shiny-server/shiny-apps/alfoutdev",
     eris_alf_dir = "/var/www/shiny-server/shiny-apps/alfresco",
-    noa_veg = "/big_scratch/mfleonawicz/Alf_Files_20121129/alf2005.cavm.merged.030212_Noatak.tif",
-    sw_veg = "/big_scratch/mfleonawicz/Alf_Files_20121129/alf2005.cavm.merged.030212.tif",
-    noa_shp = "/big_scratch/mfleonawicz/Alf_Files_20121129/noa_basin2/Noa_basin2.shp",
-    sw_shp = "/big_scratch/mfleonawicz/Alf_Files_20121129/statewide_shape/Alaska_Albers_ESRI.shp",
-    alf_extract_dir = "/atlas_scratch/mfleonawicz/alfresco"
+    noa_veg = file.path(bsalf, "alf2005.cavm.merged.030212_Noatak.tif"),
+    sw_veg = file.path(bsalf, "alf2005.cavm.merged.030212.tif"),
+    noa_shp = file.path(bsalf, "noa_basin2/Noa_basin2.shp"),
+    sw_shp = file.path(bsalf, "statewide_shape/Alaska_Albers_ESRI.shp"),
+    alf_extract_dir = alfex,
+    alf_slurm_dir = file.path(proj, "SNAPQAQC/code/alfresco") # nolint end
   )
 }
-# nolint end
