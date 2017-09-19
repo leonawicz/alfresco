2a1b4a30c3e4b10d5c2685269150127835347484
leonawicz <matt.rstat@gmail.com>
2017-09-19 13:05:13 -0600

    Refactor function, remove stats code, add inputs helper. Document.

--- R/dist.R
+++ R/dist.R
@@ -25,132 +25,135 @@
 alf_dist <- function(j, in_dir, out_dir, period, reps, n_samples = 10000,
                      density.args = list(n = 1000, adjust = 0.1, from = 0)){
   id <- basename(in_dir)
-  files.hist <- list.files(in_dir, full.names = TRUE, pattern = paste0("^", id, "__.*.CRU.*.RData$"))
-  files.all <- list.files(in_dir, full.names = TRUE, pattern=paste0("^", id, "__.*.RData$"))
-  if(period == "historical"){
-    files <- files.hist
-  } else if(period == "projected") {
-    files <- setdiff(files.all, files.hist)
-  }
-  files.locs <- sapply(strsplit(files, "__"), "[", 2)
-  locs <- unique(files.locs)
-  if(j > length(locs)) return()
-  loc <- locs[j]
-  files <- files[which(files.locs %in% loc)]
-  dat <- stat <- vector("list", length(files))
-  for(i in 1:length(files)){
-    d <- readRDS(files[i])
-    loc.grp <- d$LocGroup[1]
-    loc <- d$Location[1]
+  project <- basename(dirname(dirname(in_dir)))
+  inputs <- alf_dist_inputs(project)
+  fmo <- "FMO" %in% names(inputs)
+  if(fmo) all_fmo <- unique(inputs$FMO)
+  uloc <- unique(paste(inputs$LocGroup, inputs$Location, sep = "__"))[j]
+  uloc <- strsplit(uloc, "__")[[1]]
+  inputs <- dplyr::filter(
+    inputs,
+    .data[["Var"]] == id & .data[["LocGroup"]] == uloc[1] & .data[["Location"]] == uloc[2])
+  pat <- paste0("^", id, ".*.", uloc[1], ".*.", uloc[2], ".*.")
+  pat <- if(period == "historical") paste0(pat, "historical.*.rds$") else paste0(pat, "rcp.*.rds$")
+  files <- list.files(in_dir, full.names = TRUE, pattern = pat)
+  dat <- vector("list", length(files))
+  for(i in seq_along(files)){
+    d <- readRDS(files[i]) %>% dplyr::ungroup() %>%
+      dplyr::mutate(Vegetation = as.character(.data[["Vegetation"]]))
+    if(fmo) d <- dplyr::mutate(d, FMO = factor(.data[["FMO"]], levels = all_fmo))
     if(id != "age"){
       reps.all <- if(is.null(reps)) sort(unique(d$Replicate)) else reps
     }
     if(id == "fsv"){
+      fire_vars <- c("Fire Count", "Burn Area", "Fire Size")
       d2 <- dplyr::group_by(d, .data[["Phase"]], .data[["Scenario"]], .data[["Model"]],
-                            .data[["Location"]], .data[["Var"]], .data[["Year"]],
-                            .data[["Replicate"]], .data[["FID"]]) %>%
-        dplyr::summarise(Val = sum(.data[["Val"]])) %>% dplyr::ungroup() %>%
+                            .data[["LocGroup"]], .data[["Location"]], .data[["Var"]],
+                            .data[["Year"]], .data[["Replicate"]], .data[["FID"]])
+      if(fmo) d2 <- dplyr::group_by(d2, .data[["FMO"]], add = TRUE)
+      d2 <- dplyr::summarise(d2, Val = sum(.data[["Val"]])) %>% dplyr::ungroup() %>%
         dplyr::mutate(Vegetation = "All") # agg-veg FS
       d <- dplyr::bind_rows(d, d2) %>% dplyr::mutate( # individual and aggregate-veg fire sizes
-        Vegetation = factor(.data[["Vegetation"]], levels = unique(.data[["Vegetation"]]))) %>%
-        dplyr::group_by(.data[["Phase"]], .data[["Scenario"]], .data[["Model"]], .data[["Location"]],
-                        .data[["Var"]], .data[["Vegetation"]], .data[["Year"]])
+        Vegetation = factor(.data[["Vegetation"]], levels = unique(.data[["Vegetation"]])))
+      d <- dplyr::group_by(d, .data[["Phase"]], .data[["Scenario"]], .data[["Model"]], .data[["LocGroup"]],
+                           .data[["Location"]], .data[["Var"]], .data[["Vegetation"]], .data[["Year"]])
+      if(fmo) d <- dplyr::group_by(d, .data[["FMO"]], add = TRUE)
       d2 <- dplyr::group_by(d, .data[["Replicate"]], add = TRUE) %>%
         dplyr::summarise(BA = sum(.data[["Val"]]), FC = length(.data[["Val"]])) # burn area and fire frequency
       d2 <- d2 %>% dplyr::do(
         .,
         Expanded = suppressMessages(dplyr::right_join( # expand to include reps w/ BA and FC zero
           ., tibble::data_frame(Replicate = as.integer(reps.all)))) %>%
-          tidyr::complete(tidyr::nesting(.data[["Phase"]], .data[["Scenario"]], .data[["Model"]],
-                                  .data[["Location"]], .data[["Var"]], .data[["Vegetation"]], .data[["Year"]]),
+          tidyr::complete(tidyr::nesting(Phase, Scenario, Model, LocGroup, Location, Var, Vegetation, Year),
                           fill = list(BA = 0L, FC = 0L)) %>%
           tidyr::fill(.data[["Phase"]], .data[["Scenario"]], .data[["Model"]],
                       .data[["Location"]], .data[["Var"]], .data[["Vegetation"]], .data[["Year"]])
       ) %>% dplyr::select(.data[["Expanded"]]) %>% tidyr::unnest(.data[["Expanded"]]) %>%
-        dplyr::group_by(.data[["Phase"]], .data[["Scenario"]], .data[["Model"]], .data[["Location"]],
-                        .data[["Var"]], .data[["Vegetation"]], .data[["Year"]])
-      d <- rvtable::rvtable(d, density.args = density.args)
-      d2.ba <- dplyr::select(d2, -.data[["FC"]]) %>% dplyr::rename(Val = .data[["BA"]]) %>%
-        rvtable::rvtable(density.args = density.args) %>% dplyr::ungroup() %>%
-        dplyr::mutate(Var = "Burn Area")
-      d2.fc <- dplyr::select(d2, -.data[["BA"]]) %>% dplyr::rename(Val = .data[["FC"]]) %>%
-        rvtable::rvtable(density.args = density.args) %>% dplyr::ungroup() %>%
-        dplyr::mutate(Var = "Fire Count")
+        dplyr::ungroup()
+      d <- dplyr::ungroup(d) %>% dplyr::select(-.data[["FID"]], -.data[["Replicate"]]) %>%
+        rvtable::rvtable(discrete = TRUE)
+      d2.ba <- dplyr::select(d2, -.data[["FC"]], -.data[["Replicate"]]) %>%
+        dplyr::rename(Val = .data[["BA"]]) %>% rvtable::rvtable(discrete = TRUE) %>%
+        dplyr::mutate(Var = fire_vars[2])
+      d2.fc <- dplyr::select(d2, -.data[["BA"]], -.data[["Replicate"]]) %>%
+        dplyr::rename(Val = .data[["FC"]]) %>% rvtable::rvtable(discrete = TRUE) %>%
+        dplyr::mutate(Var = fire_vars[1])
       rm(d2)
-      d <- dplyr::bind_rows(d, d2.ba, d2.fc)
+      d <- dplyr::bind_rows(d, d2.ba, d2.fc) %>%
+        dplyr::mutate(Var = factor(.data[["Var"]], levels = fire_vars))
       rm(d2.ba, d2.fc)
       gc()
     }
-    d <- dplyr::group_by(d, .data[["Phase"]], .data[["Scenario"]], .data[["Model"]],
-                         .data[["Location"]], .data[["Var"]], .data[["Vegetation"]],
-                         .data[["Year"]])
     if(id == "age"){
       skip_veg <- c("Wetland Tundra", "Barren lichen-moss", "Temperate Rainforest")
-      d <- dplyr::filter(
-        d, !(.data[["Vegetation"]] %in% skip_veg) & sum(.data[["Freq"]]) > 30) # min 30 pixels across reps
-      if(nrow(d) == 0) return("Insufficient data.") # skip if any null df for location occurs for age
-      d <- dplyr::mutate(d, N = n()) %>% dplyr::group_by(.data[["Year"]], add = TRUE) # nolint
-      d <- rvtable::rvtable(d, density.args = density.args)
+      d <- dplyr::filter(d, !(.data[["Vegetation"]] %in% skip_veg)) %>%
+        rvtable::rvtable(discrete = TRUE)
     }
     if(id == "veg"){
-      dx <- d %>% dplyr::do(
-        .,
-        Expanded = suppressMessages(dplyr::right_join(
-          ., tibble::data_frame(Replicate = as.integer(reps.all)))) %>% # expand to include reps w/ age zero
-          tidyr::complete(tidyr::nesting(.data[["Phase"]], .data[["Scenario"]], .data[["Model"]],
-                                         .data[["LocGroup"]], .data[["Location"]], .data[["Var"]],
-                                         .data[["Vegetation"]], .data[["Year"]]),
-                          fill = list(Val = 0)) %>%
-          tidyr::fill(.data[["Phase"]], .data[["Scenario"]], .data[["Model"]],
-                      .data[["LocGroup"]], .data[["Location"]], .data[["Var"]],
-                      .data[["Vegetation"]], .data[["Year"]])
-      ) %>% dplyr::select(.data[["Expanded"]]) %>% tidyr::unnest(.data[["Expanded"]]) %>% dplyr::ungroup() %>%
-        dplyr::group_by(.data[["Phase"]], .data[["Scenario"]], .data[["Model"]],
-                        .data[["Location"]], .data[["Var"]], .data[["Vegetation"]], .data[["Year"]])
-      d <- rvtable::rvtable(d, density.args = density.args)
+      d <- dplyr::select(d, -.data[["Replicate"]]) %>%
+        rvtable::rvtable(d, discrete = TRUE)
     }
-    get_stats <- function(data){
-      data <- rvtable::sample_rvtable(data, n = n_samples, density.args = density.args)
-      dplyr::summarise(data, Mean = round(mean(.data[["Val"]])),
-                       SD = round(stats::sd(.data[["Val"]]), 1),
-                       Min = round(min(.data[["Val"]])),
-                       Pct_05 = round(stats::quantile(.data[["Val"]], 0.05)),
-                       Pct_10 = round(stats::quantile(.data[["Val"]], 0.10)),
-                       Pct_25 = round(stats::quantile(.data[["Val"]], 0.25)),
-                       Pct_50 = round(stats::quantile(.data[["Val"]], 0.50)),
-                       Pct_75 = round(stats::quantile(.data[["Val"]], 0.75)),
-                       Pct_90 = round(stats::quantile(.data[["Val"]], 0.90)),
-                       Pct_95 = round(stats::quantile(.data[["Val"]], 0.95)),
-                       Max = round(max(.data[["Val"]]))) %>%
-        dplyr::group_by(.data[["Year"]], add = TRUE)
-    }
-    s <- get_stats(d)
     dat[[i]] <- d
-    stat[[i]] <- s
     print(i)
   }
-  dir.create(statsDir <- file.path(out_dir, "stats", loc.grp, loc),
+  dir.create(dist_dir <- file.path(out_dir, "distributions", uloc[1], uloc[2]),
              recursive = TRUE, showWarnings = FALSE)
-  dir.create(samplesDir <- file.path(out_dir, "samples", loc.grp, loc),
-             recursive = TRUE, showWarnings = FALSE)
-  prefix <- if(stat[[1]]$Scenario[1] == "Historical") "historical" else "projected"
+  prefix <- if(dat[[1]]$Scenario[1] == "Historical") "historical" else "projected"
   if(id == "fsv"){
     dat <- dplyr::bind_rows(dat)
-    d_alf_fs <- dplyr::filter(dat, .data[["Var"]] == "Fire Size")
-    d_alf_ba <- dplyr::filter(dat, .data[["Var"]] == "Burn Area")
-    d_alf_fc <- dplyr::filter(dat, .data[["Var"]] == "Fire Count")
-    stats_alf_fire <- dplyr::bind_rows(stat)
-    saveRDS(d_alf_fs, file = file.path(samplesDir, paste0(prefix, "_fs.rds")))
-    saveRDS(d_alf_ba, file = file.path(samplesDir, paste0(prefix, "_ba.rds")))
-    saveRDS(d_alf_fc, file = file.path(samplesDir, paste0(prefix, "_fc.rds")))
-    saveRDS(stats_alf_fire, file = file.path(statsDir, paste0(prefix, "_fsv_stats.rds")))
+    d_alf_fs <- dplyr::filter(dat, .data[["Var"]] == "Fire Size") %>%
+      rvtable::rvtable(discrete = TRUE)
+    d_alf_ba <- dplyr::filter(dat, .data[["Var"]] == "Burn Area") %>%
+      rvtable::rvtable(discrete = TRUE)
+    d_alf_fc <- dplyr::filter(dat, .data[["Var"]] == "Fire Count") %>%
+      rvtable::rvtable(discrete = TRUE)
+    saveRDS(d_alf_fs, file = file.path(dist_dir, paste0(prefix, "_fs.rds")))
+    saveRDS(d_alf_ba, file = file.path(dist_dir, paste0(prefix, "_ba.rds")))
+    saveRDS(d_alf_fc, file = file.path(dist_dir, paste0(prefix, "_fc.rds")))
   } else {
     dat <- dplyr::bind_rows(dat)
-    stat <- dplyr::bind_rows(stat)
+    if(id == "veg") dat <- rvtable::rvtable(dat, discrete = TRUE)
+    if(id == "age") dat <- rvtable::rvtable(dat, Val = "Age", Prob = "Freq", discrete = TRUE)
     file_dat <- paste0(prefix, "_", id, ".rds")
-    file_stat <- paste0(prefix, "_", id, "_stats.rds")
-    saveRDS(dat, file = file.path(samplesDir, file_dat))
-    saveRDS(stat, file = file.path(statsDir, file_stat))
+    saveRDS(dat, file = file.path(dist_dir, file_dat))
   }
   invisible()
+}
+
+#' Table inputs from extracted data
+#'
+#' Create a data frame of inputs from extraction .rds files for probability distribution estimation.
+#'
+#' This function creates a data frame containing columns of metadata based on the .rds files
+#' stored in the \code{fsv}, \code{veg} and \code{age} extraction subdirectories.
+#' These files are output during ALFRESCO data extraction by \link{run_alf_extraction}.
+#' The columns of the data frame describe the random variable set (\code{fsv}, \code{veg} or \code{age}),
+#' location group (region set), specific location (region),
+#' model (a GCM or CRU), RCP (including "historical"), and the fire management options (FMO) treatment.
+#'
+#' If \code{in_dir} is not provided, a default is taken from \code{alfdef()$alf_extract_dir} and \code{project}, so \code{project}
+#' must be provided. \code{project} is ignored if the full project path is provided via \code{in_dir}.
+#' Provide a directory or provide a project name and rely on the default extractions directory.
+#'
+#' @param project character, see details.
+#' @param in_dir path to data extractions, the parent directory with the three subdirectories, \code{fsv}, \code{veg} and \code{age}. See details.
+#'
+#' @return a data frame.
+#' @export
+#'
+#' @examples
+#' \dontrun{alf_dist_inputs("JFSP")}
+alf_dist_inputs <- function(project, in_dir){
+  if(missing(project) & missing(in_dir))
+    stop("`project` and `in_dir` cannot both be missing.")
+  vars <- c("fsv", "veg", "age")
+  ids <- c("Var", "LocGroup", "Location", "Model", "RCP", "FMO")
+  if(missing(in_dir))
+    in_dir <- file.path(alfdef()$alf_extract_dir, project, "extractions")
+  in_dir <- file.path(in_dir, vars)
+  x <- purrr::map(in_dir, ~do.call(rbind, strsplit(list.files(file.path(.x)), "__")) %>%
+                    tibble::as_data_frame()) %>% dplyr::bind_rows()
+  names(x) <- ids[1:ncol(x)]
+  if(ncol(x) == length(ids)) x <- dplyr::mutate(x, FMO = gsub("\\.rds", "", .data[["FMO"]]))
+  x
 }
