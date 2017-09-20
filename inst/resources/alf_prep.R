06720d5fe5bed2d263921fac317bf43d9bb1192a
leonawicz <matt.rstat@gmail.com>
2017-09-20 07:37:47 -0600

    Make number of processes specific to random variable group.

--- inst/resources/alf_prep.R
+++ inst/resources/alf_prep.R
@@ -7,12 +7,15 @@ if(!exists("in_dir")) in_dir <- file.path(alfdef()$alf_extract_dir, project, "ex
 if(!exists("out_dir")) out_dir <- file.path(snapprep::snapdef()$dist_dir, "alfresco", project)
 dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
 inputs <- alf_dist_inputs(project)
-n <- length(unique(paste(inputs$LocGroup, inputs$Location)))
+n <- purrr::map_int(c("fsv", "veg", "age"), ~{
+  x <- dplyr::filter(inputs, Var == .x)
+  length(unique(paste(x$LocGroup, x$Location)))
+  })
 mc.cores <- 32
 
-mclapply(1:n, alf_dist, in_dir = file.path(in_dir, "fsv"), out_dir = out_dir,
+mclapply(1:n[1], alf_dist, in_dir = file.path(in_dir, "fsv"), out_dir = out_dir,
          period = period, reps = reps, mc.cores = mc.cores)
-mclapply(1:n, alf_dist, in_dir = file.path(in_dir, "veg"), out_dir = out_dir,
+mclapply(1:n[2], alf_dist, in_dir = file.path(in_dir, "veg"), out_dir = out_dir,
          period = period, reps = reps, mc.cores = mc.cores)
-mclapply(1:n, alf_dist, in_dir = file.path(in_dir, "age"), out_dir = out_dir,
+mclapply(1:n[3], alf_dist, in_dir = file.path(in_dir, "age"), out_dir = out_dir,
          period = period, reps = reps, mc.cores = mc.cores)
