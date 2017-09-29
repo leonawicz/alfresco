72dbdd8acef3adbf1366a4867b9cabaecd0a6008
leonawicz <matt.rstat@gmail.com>
2017-09-29 14:11:52 -0600

    Fix class check.

--- R/projections.R
+++ R/projections.R
@@ -11,7 +11,7 @@
 #' # not run
 wgs2ak <- function(xy){
   akalbers <- "+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0" # nolint
-  if(class(xy) == "matrix") xy <- tibble::data_frame(xy)
+  if("matrix" %in% class(xy)) xy <- tibble::as_data_frame(xy)
   names(xy) <- c("x", "y")
   sp::coordinates(xy) <- names(xy)
   sp::proj4string(xy) <- sp::CRS("+proj=longlat +datum=WGS84")
