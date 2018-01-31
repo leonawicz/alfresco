23306185336cab857511af94cf54e5c90e5d4315
leonawicz <matt.rstat@gmail.com>
2018-01-31 08:31:39 -0700

    Add data set documentation.

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Historical Alaska annual burn area by fire management options zones (FMOs).
+#'
+#' A data frame of annual emprical, observed historical burn areas in Alaska assembled from 1950 - 2013 fire perimiter data.
+#' The total burn area is segmented into that falling into each of six exhaustive and mutually exclusive regional zones based on existing fire management options.
+#'
+#' @format A data frame with 64 rows and 8 columns: Set (Observed), Year, and six columns providing the total burn area within each of six FMOs: Unmanaged, Limited, Modified, Full, Critical and Other.
+#' There is one row for each year.
+"obs_fmo_ba"
